7fa06f74cbe4cd49953f3166d46ca11ef795bfd1
corco <iuly_corco@yahoo.com>
2013-06-30 15:03:04 +0300

    add initial application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Visits::Application.config.secret_token = '92610a9172a188965f0f3c2bdf203d3f82f3e7bd98638a8d3d8a25edce2ad3f94adbb923820bffafdee8ef6ed988222bfa0f96131cfe9259fc970a313b6ae404'
